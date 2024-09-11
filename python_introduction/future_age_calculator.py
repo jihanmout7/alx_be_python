0fa5d1b0a1379347c1190f2987fd1fb26b532cea
Jihane <moutalebjihane90@gmail.com>
2024-09-11 23:48:33 +0100

    first commit

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+# Prompt the user for their current age
+# Define variables
+age = 30
+year = 27
+
+# Calculate the user age in 2050 This year is 2023
+addition_result = age + year
+# print result
+print(f"In 2050, you will be {addition_result} years old.")
