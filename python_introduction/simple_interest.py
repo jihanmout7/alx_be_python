06a5e15feab8917100fef2c704302eda5525a87c
Jihane <moutalebjihane90@gmail.com>
2024-09-11 22:54:00 +0100

    firt commit

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+# Define the variables
+principal = 1000
+rate = 0.05
+time = 3
+# The formula for the simple interest
+# (I) is the interest earned,
+# (P) is the principal amount(initial investment),
+# (R) is the annual interest rate(as a decimal),
+# (T) is the time the money is invested for in years.
+interest = principal * rate * time
+
+# Print the results in the specified format
+print(f"The simple interest is: {interest}")
