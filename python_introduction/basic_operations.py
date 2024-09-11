a720b9e66279f395795cb3a2859f4ef21b5571e1
Jihane <moutalebjihane90@gmail.com>
2024-09-11 22:32:44 +0100

    firt commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define the variables
+number1 = 10
+number2 = 5
+
+# Perform the calculations
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+# Print the results in the specified format
+print(f"Addition of {number1} and {number2} is {addition_result}.")
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}.")
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}.")
