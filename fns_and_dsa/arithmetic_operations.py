08be2a4287d7cfaf772493a3adb831a9b083bf54
jihane <moutalebjihane90@gmail.com>
2024-10-05 23:38:31 +0100

    first commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,28 @@
-def perform_operation("num1,num2,operation"):
+def perform_operation():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
     try:
-        if num1 == 0:
-            print('Error divsion by zero')
-    except :
-        print('Error')
+        if operation == "add":
+            result = num1 + num2
+        elif operation == "subtract":
+            result = num1 - num2
+        elif operation == "multiply":
+            result = num1 * num2
+        elif operation == "divide":
+            if num2 == 0:
+                print('Error: Division by zero.')
+                return  # Exit the function early
+            result = num1 / num2
+        else:
+            print('Error: Invalid operation.')
+            return  # Exit the function early
 
+        print(f'The result of {operation} {num1} and {num2} is: {result}')
+
+    except Exception as e:
+        print('Error:', e)
+
+# Call the function
 perform_operation()
-
-
