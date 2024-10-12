0abb58ec0c40b1ff148bacf7b2b2eecd7eaf08ee
Jihane <moutalebjihane90@gmail.com>
2024-10-13 00:52:32 +0100

    first commit

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,16 +1,37 @@
-class Book :
-    def __init__(self, title, author, year ):
-        self.title = title
-        self.author = author
-        self.year = year
+class Book:
+        """
+        A class representing a book with title, author, and publication year.
+        """
 
-    def __del__(self):
-        print (f"Deleting {self.title}")
+        def __init__(self, title, author, year):
+            """
+            Initializes a Book instance with the provided title, author, and year.
+            """
+            self.title = title
+            self.author = author
+            self.year = year
 
-    def __str__(self):
-        
-        return f"{self.title} by {self.author}, published in {self.year}"
+        def __str__(self):
+            """
+            Returns a string representation of the book in the format "(title) by (author), published in (year)".
+            """
+            return f"{self.title} by {self.author}, published in {self.year}"
 
-    def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+        def __repr__(self):
+            """
+            Returns a string that can be used to recreate the Book instance.
+            """
+            return f"Book('{self.title}', '{self.author}', {self.year})"
+
+        def __del__(self):
+            """
+            Prints a message indicating the book is being deleted. (Destructor)
+            **Note:** Destructors are not guaranteed to be called in Python.
+            """
+            print(f"Deleting {self.title}")
+
+    # Example usage
+    # my_book = Book("1984", "George Orwell", 1949)
+    # print(my_book)
+    # print(repr(my_book))
 
